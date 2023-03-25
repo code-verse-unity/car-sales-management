00d129ed7e53375506f49c2811cba712821ede87
Fitia Ismael Rakotondrazaka <88872262+Ismael-Rakotondrazaka@users.noreply.github.com>
2023-03-25 11:39:14 +0300

    fix/ Fix not locked and raw in IndexClientUseCase::execute

--- src/Data/UseCases/Clients/IndexClientUseCase.php
+++ src/Data/UseCases/Clients/IndexClientUseCase.php
@@ -3,11 +3,9 @@
 namespace App\Data\UseCases\Clients;
 
 use App\Core\Utils\Failures\ServerFailure;
-use App\Core\Utils\Strings\RandomString;
 use App\Domain\Repositories\ClientRepositoryInterface;
 use App\Data\Models\ClientModel;
 use App\Core\Utils\Failures\Failure;
-use DateTime;
 
 class IndexClientUseCase
 {
@@ -21,18 +19,10 @@ class IndexClientUseCase
   public function execute()
   {
     try {
-      $randomStringGenerator = new RandomString(ClientModel::ID_CHARACTERS);
-      $id = $randomStringGenerator->generate(ClientModel::ID_LENGTH);
-
-      // $client = new ClientModel($id, $firstName, $lastName, $email, $password, null, null, null);
-
-      return $this->clientRepository->findAll();
-
-      // ! lock to make read only
-      // $client->lock();
-
-      // return $client->getRaw();
-
+      return array_map(function ($client) {
+         $client->lock();
+         return $client->getRaw();
+      }, $this->clientRepository->findAll());
     } catch (\Throwable $th) {
       if ($th instanceof Failure) {
         return $th;
