041a94d064297af2ef127e3e2e5a8861da6ef949
Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2023-03-25 21:45:39 +0300

    Feat: Add validators for Client entity

--- src/Domain/Entities/Client.php
+++ src/Domain/Entities/Client.php
@@ -2,37 +2,114 @@
 
 namespace App\Domain\Entities;
 
+use App\Core\Utils\Failures\ServerFailure;
 use DateTime;
-use Exception;
 
 abstract class Client implements EntityInterface
 {
   const ID_LENGTH = 21;
   const ID_CHARACTERS = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_";
+  const NAME_MIN_LENGTH = 1;
+  const CONTACT_MIN_LENGTH = 1;
 
-  private string $id;
-  private string $name;
-  private string $contact;
-  private DateTime $createdAt;
-  private DateTime $updatedAt;
+  private $id;
+  private $name;
+  private $contact;
+  private $createdAt;
+  private $updatedAt;
 
-  private array $errors;
+  private array $errors = [];
   private bool $locked = false;
 
-  public function __construct($id, string $name, string $contact, DateTime $createdAt, DateTime $updatedAt)
+  /*
+  ! It accepts any type of parameter,
+  ! but it validate them when creating a new instance
+  ! It doesn't throw an exception, but instead, it store in errors
+  ! So use the hasErrors method and set fix the error before saving in a repository
+  ! If id, createdAt, updatedAt are not valid, mostly it's an error from the developers,
+  ! and there is no way to fix it from through this class,
+  ! instead fix the errors and create a new instance
+  */
+  public function __construct($id, $name, $contact, $createdAt, $updatedAt)
   {
     $this->id = $this->validateId($id);
-
-    $this->name = $name;
-    $this->contact = $contact;
-
-    $this->createdAt = $createdAt;
-    $this->updatedAt = $updatedAt;
+    $this->name = $this->validateName($name);
+    $this->contact = $this->validateContact($contact);
+    $this->createdAt = $this->validateCreatedAt($createdAt);
+    $this->updatedAt = $this->validateUpdatedAt($updatedAt);
   }
 
   private function validateId($id)
   {
+    if (!is_string($id)) {
+      $this->addErrorByAttribute("id", "L'identifiant d'un client doit être une chaîne de caractères."); // TODO set to more human readable message
+    }
+
+    if (strlen($id) !== self::ID_LENGTH) {
+      $this->addErrorByAttribute("id", "L'identifiant d'un client doit avoir" . self::ID_LENGTH . " caractères.");
+    }
+
+    if (!$this->containsOnlyChars($id, str_split(self::ID_CHARACTERS))) {
+      $this->addErrorByAttribute("id", "L'identifiant d'un client contient des caractères non autorisées.");
+    }
+
     return $id;
+  }
+
+  private function validateName($name)
+  {
+    $trimmed = trim($name);
+
+    if (!isset($name) || $name === null || empty($trimmed)) {
+      $this->addErrorByAttribute("name", "Le nom du client est obligatoire.");
+    }
+
+    if (!is_string($name)) {
+      $this->addErrorByAttribute("name", "Le nom du client doit être une chaîne de caractères.");
+    }
+
+    if (strlen($trimmed) < self::NAME_MIN_LENGTH) {
+      $this->addErrorByAttribute("name", "La longueur minimale pour le nom du client est de " . self::NAME_MIN_LENGTH . " caractères.");
+    }
+
+    return $trimmed;
+  }
+
+  private function validateContact($contact)
+  {
+    $trimmed = trim($contact);
+
+    if (!isset($contact) || $contact === null || empty($trimmed)) {
+      $this->addErrorByAttribute("contact", "Le contact du client est obligatoire.");
+    }
+
+    if (!is_string($contact)) {
+      $this->addErrorByAttribute("contact", "Le contact du client doit être une chaîne de caractères.");
+    }
+
+    if (strlen($trimmed) < self::CONTACT_MIN_LENGTH) {
+      $this->addErrorByAttribute("contact", "La longueur minimale pour le contact du client est de " . self::CONTACT_MIN_LENGTH . " caractères.");
+    }
+
+    return $trimmed;
+  }
+
+  private function validateCreatedAt($createdAt)
+  {
+    if (!$createdAt instanceof Datetime) {
+      $this->addErrorByAttribute("createdAt", "La date de creation du client n'est pas valide.");
+    }
+
+    return $createdAt;
+  }
+
+  private function validateUpdatedAt($updatedAt)
+  {
+    if (!$updatedAt instanceof Datetime) {
+      $this->addErrorByAttribute("updatedAt", "La date de creation du client n'est pas valide.");
+    }
+
+    return $updatedAt;
   }
 
   public function getId()
@@ -48,10 +125,13 @@ abstract class Client implements EntityInterface
   public function setName($name)
   {
     if ($this->locked) {
-      throw new Exception("instance locked");
+      throw new ServerFailure("instance locked");
     }
 
-    $this->name = $name;
+    // we remove existing errors and validate
+    $this->removeErrorsByAttribute("name");
+    $this->name = $this->validateName($name);
+
     $this->triggerUpdate();
   }
 
@@ -68,10 +148,12 @@ abstract class Client implements EntityInterface
   public function setContact($contact)
   {
     if ($this->locked) {
-      throw new Exception("instance locked");
+      throw new ServerFailure("instance locked");
     }
 
-    $this->contact = $contact;
+    $this->removeErrorsByAttribute("contact");
+    $this->contact = $this->validateContact($contact);
+
     $this->triggerUpdate();
   }
 
@@ -126,8 +208,37 @@ abstract class Client implements EntityInterface
     return $this->errors[$attribute];
   }
 
+  /*
+  if the attribute does not exist in the errors, it will throw a TypeError
+  so use hasError($attribute) before using it
+  */
   public function getFirstError(string $attribute): string
   {
     return $this->errors[$attribute][0];
+  }
+
+  protected function addErrorByAttribute(string $attribute, string $message)
+  {
+    if (array_key_exists($attribute, $this->errors)) {
+      $this->errors[$attribute][] = $message;
+    } else {
+      $this->errors[$attribute] = [$message];
+    }
+  }
+
+  private function containsOnlyChars(string $string, array $allowedChars)
+  {
+    $length = strlen($string);
+    for ($i = 0; $i < $length; $i++) {
+      if (!in_array($string[$i], $allowedChars)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private function removeErrorsByAttribute(string $attribute)
+  {
+    unset($this->errors[$attribute]);
   }
 }
