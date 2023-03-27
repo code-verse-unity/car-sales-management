7af01f9714e9da1e749d8ed029fe57fb969a21a6
Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2023-03-27 19:03:12 +0300

    Feat: Add edit car feature

--- public/index.php
+++ public/index.php
@@ -27,6 +27,7 @@ use App\Presentation\Controllers\Cars\ShowCarController;
 use App\Presentation\Controllers\Cars\CreateCarController;
 use App\Presentation\Controllers\Cars\IndexCarController;
 use App\Presentation\Controllers\Cars\UpdateCarController;
+use App\Presentation\Controllers\Cars\EditCarController;
 
 $dotenv = Dotenv::createImmutable(__DIR__ . "/../");
 $dotenv->load();
@@ -74,6 +75,8 @@ $indexCarController = new IndexCarController($indexCarUseCase);
 // UpdateCarUseCase and UpdateCarController
 $updateCarUseCase = new UpdateCarUseCase($carRepository);
 $updateCarController = new UpdateCarController($updateCarUseCase);
+// EditCarController
+$editCarController = new EditCarController($showCarUseCase);
 
 $app = new Application();
 
@@ -94,6 +97,7 @@ $app->router->put("/clients/{clientId}", [$updateClientController, "execute"]);
 $app->router->get("/cars", [$indexCarController, "execute"]); // with search functionality
 $app->router->post("/cars/create", [$storeCarController, "execute"]);
 $app->router->get("/cars/create", [$createCarController, "execute"]);
+$app->router->get("/cars/{carId}/edit", [$editCarController, "execute"]);
 $app->router->put("/cars/{carId}/edit", [$updateCarController, "execute"]); // ! this one works on postman, but not in the browser because there is no PUT method from html
 $app->router->post("/cars/{carId}/edit", [$updateCarController, "execute"]); // ! this one can works on both, and doesn't have any collision with other paths, note the POST method
 $app->router->get("/cars/{carId}", [$showCarController, "execute"]);
