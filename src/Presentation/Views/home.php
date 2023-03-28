e125a3e458bd97e27dc45d22c65a4d465661d7af
Rasolomanana Herimanitra Olivier <herimanitraolivierr@gmail.com>
2023-03-28 15:03:16 +0300

    feat:add home controller

--- src/Presentation/Views/home.php
+++ src/Presentation/Views/home.php
@@ -1,11 +1,14 @@
 <?php
 
 ?>
-
-<h1>
-  <?= print_r($clients); ?>
-</h1>
-
-<ul>
-  hello
-</ul>
+<main class="py-2">
+  <div class="row py-4">
+    <div class="col d-flex flex-column">
+      <h1>Bienvenue dans le tableau de bord.</h1>
+      <h5>Activités</h5>
+    </div>
+    <div class="col">
+      Image
+    </div>
+  </div>
+</main>
