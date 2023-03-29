c3c47b192455a7a4dc2727fcf38243692714da52
Rasolomanana Herimanitra Olivier <herimanitraolivierr@gmail.com>
2023-03-29 13:34:10 +0300

    Delete unused image files and remove related CSS styles and HTML elements

--- src/Presentation/Views/home.php
+++ src/Presentation/Views/home.php
@@ -3,26 +3,12 @@
 ?>
 <main class="py-2">
   <div class="row py-4">
-    <div class="col d-flex flex-column justify-content-between">
-      <h1><span class="text-orange">Bienvenue</span> dans le tableau de bord.</h1>
+    <div class="col d-flex flex-column">
+      <h1>Bienvenue dans le tableau de bord.</h1>
       <h5>Activités</h5>
     </div>
     <div class="col">
-      <div class="ca-image d-flex flex-column justify-content-center p-4 text-light">
-        <h3>Chiffre d'affaires ( 6 derniers mois ) </h3>
-        <div class="fw-bold display-4">125 000 000 Ar</div>
-      </div>
+      Image
     </div>
-  </div>
-
-  <div class="row">
-    <div class="col">
-      <div class='clients-image d-flex flex-column justify-content-center p-4 text-light'>
-        <h4>Liste des Clients </h4>
-        <div class="fw-bold display-4">125</div>
-      </div>
-    </div>
-    <div class="col">test 2</div>
-    <div class="col">test 3</div>
   </div>
 </main>
