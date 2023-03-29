bf7e373e6e4162895061ec102b28e2a23d85c8a7
Rasolomanana Herimanitra Olivier <herimanitraolivierr@gmail.com>
2023-03-29 14:13:15 +0300

    Add hover effect and click navigation to /clients, /cars, and /orders on home page

--- src/Presentation/Views/home.php
+++ src/Presentation/Views/home.php
@@ -4,7 +4,7 @@
 <main class="py-2">
   <div class="row py-4">
     <div class="col d-flex flex-column justify-content-between">
-      <h1>Bienvenue dans le tableau de bord.</h1>
+      <h1><span class="text-orange">Bienvenue</span> dans le tableau de bord.</h1>
       <h5>Activités</h5>
     </div>
     <div class="col">
@@ -30,9 +30,28 @@
     </div>
     <div class="col">
       <div class='cars-image d-flex flex-column justify-content-center p-4 text-light'>
-        <h4>Liste des produits </h4>
+        <h4>Liste des voitures </h4>
         <div class="fw-bold display-4">120</div>
       </div>
     </div>
   </div>
 </main>
+
+<!-- Js script to handle navigation to /clients /cars and /orders -->
+<script>
+  const clients = document.querySelector('.clients-image');
+  const cars = document.querySelector('.cars-image');
+  const orders = document.querySelector('.orders-image');
+
+  clients.addEventListener('click', () => {
+    window.location.href = '/clients';
+  });
+
+  cars.addEventListener('click', () => {
+    window.location.href = '/voitures';
+  });
+
+  orders.addEventListener('click', () => {
+    window.location.href = '/achats';
+  });
+</script>
