af1d781b3db28e0072be40eba92d9c8ccaca074f
Rasolomanana Herimanitra Olivier <herimanitraolivierr@gmail.com>
2023-03-30 17:08:26 +0300

    feat: add client and car list views with styling and actions

--- src/Presentation/Views/cars/index.php
+++ src/Presentation/Views/cars/index.php
@@ -3,7 +3,7 @@
 ?>
 
 <!-- Search functionality, already handled by IndexCarUseCase and IndexCarController -->
-<form action="/cars">
+<!-- <form action="/cars">
     <input type="search" name="name" id="id" value="<?= $nameQuery ?? "" ?>">
 
     <input type="submit" value="Search">
@@ -11,17 +11,17 @@
     <a href="/cars">Effacer</a>
 </form>
 
-<?php if (count($cars) === 0): ?>
+<?php if (count($cars) === 0) : ?>
     <p>
-        <?php if ($nameQuery): ?>
+        <?php if ($nameQuery) : ?>
             Il n'y a pas de voitures correspondant à votre recherche.
-        <?php else: ?>
+        <?php else : ?>
             Il n'y a pas encore de voitures.
         <?php endif; ?>
     </p>
-<?php else: ?>
+<?php else : ?>
     <ul>
-        <?php foreach ($cars as $car): ?>
+        <?php foreach ($cars as $car) : ?>
             <li>
                 <pre>
                     <?php
@@ -33,4 +33,41 @@
             </li>
         <?php endforeach; ?>
     </ul>
-<?php endif; ?>
+<?php endif; ?> -->
+
+<?php
+
+?>
+
+<main class="py-4">
+    <div class="d-flex justify-content-between align-items-center">
+        <h1 class="display-4 fw-bold">Liste des <span class="text-orange">Voitures</span>.</h1>
+        <div>
+            <button class="btn btn-primary">Ajouter</button>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row pt-3">
+            <div class="col">Désignation</div>
+            <div class="col">Prix (Ar)</div>
+            <div class="col">Nombre en stock</div>
+            <div class="col d-flex justify-content-end">Actions</div>
+        </div>
+
+        <div class="row my-3 py-3 px-2 bg-white rounded-3 shadow-sm">
+            <div class="col d-flex align-items-center fw-bold">Ferrari M22</div>
+            <div class="col d-flex align-items-center">82 000 000</div>
+            <div class="col d-flex align-items-center">8</div>
+            <div class="col d-flex justify-content-end">
+                <button class="btn btn-primary mx-3">
+                    <img src="assets/icons/edit.svg" alt="edit cars" srcset="" class="icon">
+                </button>
+                <button class="btn btn-danger">
+                    <img src="assets/icons/delete.svg" alt="delete cars" srcset="" class="icon">
+                </button>
+            </div>
+        </div>
+    </div>
+
+</main>
--- src/Presentation/Views/clients/index.php
+++ src/Presentation/Views/clients/index.php
@@ -2,11 +2,33 @@
 
 ?>
 
-<h1>
-  <?= print_r($clients); ?>
-  Liste des clients
-</h1>
+<main class="py-4">
+  <div class="d-flex justify-content-between align-items-center">
+    <h1 class="display-4 fw-bold">Liste des <span class="text-orange">Clients</span>.</h1>
+    <div>
+      <button class="btn btn-primary">Ajouter</button>
+    </div>
+  </div>
 
-<ul>
-  hello
-</ul>
+  <div class="container">
+    <div class="row pt-3">
+      <div class="col">Nom et Prénom(s)</div>
+      <div class="col">Contact</div>
+      <div class="col d-flex justify-content-end">Actions</div>
+    </div>
+
+    <div class="row my-3 py-3 px-2 bg-white rounded-3 shadow-sm">
+      <div class="col d-flex align-items-center fw-bold">John Doe</div>
+      <div class="col d-flex align-items-center">+23648464548</div>
+      <div class="col d-flex justify-content-end">
+        <button class="btn btn-primary mx-3">
+          <img src="assets/icons/edit.svg" alt="edit cars" srcset="" class="icon">
+        </button>
+        <button class="btn btn-danger">
+          <img src="assets/icons/delete.svg" alt="delete cars" srcset="" class="icon">
+        </button>
+      </div>
+    </div>
+  </div>
+
+</main>
