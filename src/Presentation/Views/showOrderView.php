ee8e3f62616065e9432c3b3d6edc00eb37ca6d62
Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2023-03-31 00:20:51 +0300

    Chore: Add form to test the destroy order feature

--- src/Presentation/Views/showOrderView.php
+++ src/Presentation/Views/showOrderView.php
@@ -4,4 +4,8 @@
 
 <pre>
     <?php print_r($order) ?>
+
+    <form action="/orders/<?= $order["id"] ?>/delete" method="POST">
+        <input type="submit" value="Annuler cet achat" style="color:red;">
+    </form>
 </pre>
