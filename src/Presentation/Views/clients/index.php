5bf75218d1a876d362e10fe0c96163af8c308964
Rasolomanana Herimanitra Olivier <herimanitraolivierr@gmail.com>
2023-03-31 08:54:28 +0300

    refactor(views): add conditional rendering for clients list The clients list is now conditionally rendered based on whether there are any clients to display. If there are no clients, a message is displayed indicating that there are no clients to display. If there are clients, they are displayed in a loop. This improves the user experience by providing more context when there are no clients to display.

--- src/Presentation/Views/clients/index.php
+++ src/Presentation/Views/clients/index.php
@@ -1,5 +1,4 @@
 <?php
-
 ?>
 
 <main class="py-4">
@@ -17,18 +16,27 @@
       <div class="col d-flex justify-content-end">Actions</div>
     </div>
 
-    <div class="row my-3 py-3 px-2 bg-white rounded-3 shadow-sm">
-      <div class="col d-flex align-items-center fw-bold">John Doe</div>
-      <div class="col d-flex align-items-center">+23648464548</div>
-      <div class="col d-flex justify-content-end">
-        <button class="btn btn-primary mx-3">
-          <img src="assets/icons/edit.svg" alt="edit cars" srcset="" class="icon">
-        </button>
-        <button class="btn btn-danger">
-          <img src="assets/icons/delete.svg" alt="delete cars" srcset="" class="icon">
-        </button>
+
+    <?php if (empty($clients)) : ?>
+      <div class="row my-3 py-3 px-2 bg-white rounded-3 shadow-sm">
+        <div class="col d-flex align-items-center fw-bold">Aucun client enregistré</div>
       </div>
-    </div>
+    <?php else : ?>
+      <?php foreach ($clients as $client) : ?>
+        <div class="row my-3 py-3 px-2 bg-white rounded-3 shadow-sm">
+          <div class="col d-flex align-items-center fw-bold"><?= $client['name'] ?></div>
+          <div class="col d-flex align-items-center"><?= $client['contact'] ?></div>
+          <div class="col d-flex justify-content-end">
+            <button class="btn btn-primary mx-3">
+              <img src="assets/icons/edit.svg" alt="edit cars" srcset="" class="icon">
+            </button>
+            <button class="btn btn-danger">
+              <img src="assets/icons/delete.svg" alt="delete cars" srcset="" class="icon">
+            </button>
+          </div>
+        </div>
+      <?php endforeach; ?>
+    <?php endif; ?>
   </div>
 
 </main>
