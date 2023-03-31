71fce00359c68cefc4c636876b4774cf81c8c95c
Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2023-03-31 20:18:20 +0300

    Fix: Add NotFoundFailure if the bill pdf is not found

--- src/Data/UseCases/Bills/DownloadBillUseCase.php
+++ src/Data/UseCases/Bills/DownloadBillUseCase.php
@@ -2,6 +2,7 @@
 
 namespace App\Data\UseCases\Bills;
 
+use App\Core\Utils\Failures\NotFoundFailure;
 use App\Domain\Repositories\OrderRepositoryInterface;
 use App\Core\Utils\Failures\ServerFailure;
 use App\Core\Utils\Failures\Failure;
@@ -23,11 +24,17 @@ class DownloadBillUseCase
 
             $filename = "facture_" . $order->getId() . ".pdf";
 
-            header("Content-Type: application/pdf");
-            header("Content-Disposition: attachement; filename=\"" . $filename . "\"");
+            $path = __DIR__ . "/../../../../private/bills/$filename";
 
-            require_once __DIR__ . "/../../../../private/bills/$filename";
-            exit;
+            if (!file_exists($path)) {
+                throw new NotFoundFailure();
+            } else {
+                header("Content-Type: application/pdf");
+                header("Content-Disposition: attachement; filename=\"" . $filename . "\"");
+                require_once $path;
+                exit;
+            }
+
         } catch (\Throwable $th) {
             if ($th instanceof Failure) {
                 return $th;
