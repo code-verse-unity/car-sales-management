ee50a90ca8782f9a2c116efb3bf0a285c17974c3
Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2023-03-31 21:26:45 +0300

    Feat: Remove the bill when destroying a order

--- src/Data/UseCases/Orders/DestroyOrderUseCase.php
+++ src/Data/UseCases/Orders/DestroyOrderUseCase.php
@@ -3,6 +3,7 @@
 namespace App\Data\UseCases\Orders;
 
 use App\Core\Utils\Failures\ServerFailure;
+use App\Data\UseCases\Bills\DestroyBillUseCase;
 use App\Domain\Repositories\OrderRepositoryInterface;
 use App\Core\Utils\Failures\Failure;
 
@@ -20,7 +21,16 @@ class DestroyOrderUseCase
     public function execute($orderId)
     {
         try {
-            $this->orderRepository->delete($orderId);
+            $order = $this->orderRepository->findById($orderId);
+
+            $this->orderRepository->delete($order->getId());
+
+            $destroyBillUseCase = new DestroyBillUseCase();
+            $useCaseResult = $destroyBillUseCase->execute($order->getId());
+
+            if ($useCaseResult instanceof Failure) {
+                return $useCaseResult;
+            }
         } catch (\Throwable $th) {
             if ($th instanceof Failure) {
                 return $th;
