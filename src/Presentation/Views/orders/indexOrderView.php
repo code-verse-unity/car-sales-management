fefb1df9437ebcf4a77ca6edeaa84ce8ada73d2c
Rasolomanana Herimanitra Olivier <herimanitraolivierr@gmail.com>
2023-04-01 16:42:01 +0300

    fix(views): reinforce client validations

--- src/Presentation/Views/orders/indexOrderView.php
+++ src/Presentation/Views/orders/indexOrderView.php
@@ -3,52 +3,6 @@
 ?>
 
 
-<script>
-    // ! this script only works because the form's method is GET, so we can use window.location.href
-    const startAt = document.getElementById("startAt");
-    const endAt = document.getElementById("endAt");
-    const form = document.getElementById("index-order-form");
-    const basePath = "/orders";
-
-    form.addEventListener("submit", (event) => {
-        event.preventDefault();
-        const startAtValue = startAt.value;
-        const endAtValue = endAt.value;
-        let path = basePath + "?";
-
-        if (startAtValue) {
-            path += `startAt=${startAtValue}&`;
-        }
-
-        if (endAtValue) {
-            path += `endAt=${endAtValue}&`;
-        }
-
-        window.location.href = path;
-    });
-</script>
-
-<!-- <?php if (count($orders) === 0) : ?>
-    <p>
-        <?php if ($starAt || $endAt) : ?>
-            Il n'y a pas d'achats correspondant à votre recherche.
-        <?php else : ?>
-            Il n'y a pas encore d'achats effectués.
-        <?php endif; ?>
-    </p>
-<?php else : ?>
-    <ul>
-        <?php foreach ($orders as $order) : ?>
-            <li>
-                <pre>
-                            <?php print_r($order) ?>
-                        </pre>
-
-                <a href="<?= "/orders/" . $order["id"] ?>">Voir les détails</a>
-            </li>
-        <?php endforeach; ?>
-    </ul>
-<?php endif; ?> -->
 
 <main class="py-4">
     <div class="d-flex justify-content-between align-items-center">
@@ -57,46 +11,151 @@
             <button class="btn btn-primary">Ajouter</button>
         </div>
     </div>
-    <!-- Search functionality, already handled by IndexOrderUseCase and IndexOrderController -->
-    <form action="/orders" method="GET" id="index-order-form" class="container d-flex py-3 gap-3">
-        <div>
-            <label for="startAt" class="py-1">Début</label>
-            <input type="date" name="startAt" id="startAt" value="<?= $startAt ? $startAt->format("Y-m-d") : "" ?>" class="form-control">
+
+    <div class="py-3">
+        <form action="/orders" method="GET" id="search" class="container d-flex gap-3" novalidate>
+            <div>
+                <label for="startAt" class="py-1">Début</label>
+                <input type="date" name="startAt" id="startAt" value="<?= $startAt ? $startAt->format("Y-m-d") : "" ?>" class="form-control">
+
+
+            </div>
+
+            <div>
+                <label for="endAt" class="py-1">Fin</label>
+                <input type="date" name="endAt" id="endAt" value="<?= $endAt ? $endAt->format("Y-m-d") : "" ?>" class="form-control">
+            </div>
+
+            <div class="align-self-end">
+                <button type="submit" class="btn btn-primary">Rechercher</button>
+            </div>
+
+            <div class="align-self-end ml-2">
+                <a href="/orders">
+                    <button class="btn" type="button">
+                        Effacer
+                    </button>
+                </a>
+            </div>
+        </form>
+        <div class="invalid-feedback container ">
+            Veuillez spécifier une date de début et de fin.
         </div>
-
-        <div>
-            <label for="endAt" class="py-1">Fin</label>
-            <input type="date" name="endAt" id="endAt" value="<?= $endAt ? $endAt->format("Y-m-d") : "" ?>" class="form-control">
-        </div>
-
-        <div class="align-self-end">
-            <input type="submit" value="Rechercher" class="btn btn-primary">
-        </div>
-
-        <div class="align-self-end ml-2">
-            <a href="/orders">
-                <button class="btn">
-                    Effacer
-                </button>
-            </a>
-        </div>
-    </form>
-
-    <div class="container">
-        <div class="row pt-3">
-            <div class="col">Identifiant</div>
-            <div class="col">Nom du client</div>
-            <div class="col">Voitures</div>
-            <div class="col">Quantité</div>
-            <div class="col d-flex justify-content-end">Actions</div>
-        </div>
+    </div>
 
 
-        <?php if (empty($orders)) : ?>
+
+    <script>
+        // ! this script only works because the form's method is GET, so we can use window.location.href
+        const startAt = document.getElementById("startAt");
+        const endAt = document.getElementById("endAt");
+        const form = document.getElementById("search");
+        const feedback = document.getElementsByClassName('invalid-feedback');
+        const basePath = "/orders";
+
+        startAt.addEventListener('input', (e) => {
+            if (e.target.value === '') {
+                startAt.classList.add("is-invalid");
+            } else {
+                startAt.classList.remove("is-invalid");
+                if (endAt.value !== '') {
+                    endAt.classList.remove("is-invalid");
+                    feedback[0].style.display = 'none';
+                }
+            }
+        })
+
+
+        endAt.addEventListener('input', (e) => {
+            if (e.target.value === '') {
+                endAt.classList.add("is-invalid");
+            } else {
+                endAt.classList.remove("is-invalid");
+                if (startAt.value !== '') {
+                    startAt.classList.remove("is-invalid");
+                    feedback[0].style.display = 'none';
+                }
+            }
+        })
+
+
+        form.addEventListener("submit", (event) => {
+            event.preventDefault();
+            const startAtValue = startAt.value;
+            const endAtValue = endAt.value;
+
+            const inputs = form.querySelectorAll("input");
+            Array.from(inputs).forEach(input => {
+                if (input.value === '') {
+                    input.classList.add("is-invalid");
+                }
+            })
+
+            const hasError = Array.from(inputs).some((input) => input.classList.contains("is-invalid"));
+
+            if (hasError) {
+                event.preventDefault();
+                feedback[0].style.display = 'block';
+            } else {
+                if (startAtValue && endAtValue) {
+                    const startAtDate = new Date(startAtValue);
+                    const endAtDate = new Date(endAtValue);
+
+                    if (startAtDate > endAtDate) {
+                        event.preventDefault();
+                        feedback[0].style.display = 'block';
+                        feedback[0].innerHTML = "La date de début doit être inférieure à la date de fin.";
+
+                        Array.from(inputs).forEach(input => {
+                            console.log('called');
+                            input.classList.add("is-invalid");
+                        })
+                        return;
+                    }
+                }
+
+
+                feedback[0].style.display = 'none';
+                let path = basePath + "?";
+
+                if (startAtValue) {
+                    path += `startAt=${startAtValue}&`;
+                }
+
+                if (endAtValue) {
+                    path += `endAt=${endAtValue}&`;
+                }
+
+                window.location.href = path;
+            }
+
+
+        });
+    </script>
+
+
+
+
+
+    <?php if (empty($orders)) : ?>
+        <?php if ($starAt || $endAt) : ?>
+            <div class="d-flex justify-content-center p-5">
+                <h3 class="text-center">Il n'y a pas d'achats correspondant à votre recherche.</h3>
+            </div>
+        <?php else : ?>
             <div class="d-flex justify-content-center p-5">
                 <h3 class="text-center">Aucun achat n'a été enregistré.</h3>
             </div>
-        <?php else : ?>
+        <?php endif; ?>
+    <?php else : ?>
+        <div class="container">
+            <div class="row pt-3">
+                <div class="col">Identifiant</div>
+                <div class="col">Nom du client</div>
+                <div class="col">Voitures</div>
+                <div class="col">Quantité</div>
+                <div class="col d-flex justify-content-end">Actions</div>
+            </div>
             <?php foreach ($orders as $order) : ?>
                 <div class="row my-3 py-3 px-2 bg-white rounded-3 shadow-sm">
                     <div class="col d-flex align-items-center fw-bold"><?= $order['id'] ?></div>
@@ -124,7 +183,8 @@
                     <button class="btn btn-link">Télécharger la facture.</button>
                 </div>
             <?php endforeach; ?>
-        <?php endif; ?>
-    </div>
+        </div>
+    <?php endif; ?>
+
 
 </main>
