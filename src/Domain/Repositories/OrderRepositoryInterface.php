f63c78c82bdf9fb8b5b43f9a7d7e17bcdc7f899f
Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2023-04-02 17:45:13 +0300

    Feat: Add count and revenue methods to OrderRepositoryInterface * Add getCount method * Add getCountByLastMonths method * Add getRevenue method * Add getRevenueByLastMonths method

--- src/Domain/Repositories/OrderRepositoryInterface.php
+++ src/Domain/Repositories/OrderRepositoryInterface.php
@@ -9,6 +9,10 @@ interface OrderRepositoryInterface
 {
     public function findAll(?DateTime $startAt, ?DateTime $endAt): array;
     public function findById(string $id): Order;
+    public function getCount(): int;
+    public function getCountByLastMonths(int $lastMonths): int;
+    public function getRevenue(): int;
+    public function getRevenueByLastMonths(int $lastMonths): int;
     public function findByClientId(string $clientId): array;
     // public function findByCarId(string $carId): array; // require more time to implement and maybe not necessary
     public function save(string $id, string $clientId, array $orderCarsIds, array $carsIds, array $quantities, string $createdAt, string $updatedAt): void;
