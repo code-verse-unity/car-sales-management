32269189eefe5e3e243f17d439e055a423c99714
Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2023-04-02 17:31:26 +0300

    Feat: Add getCount method to CarSourceInterface

--- src/Data/Sources/Cars/CarSourceInterface.php
+++ src/Data/Sources/Cars/CarSourceInterface.php
@@ -8,6 +8,7 @@ interface CarSourceInterface
 {
     public function findAll(): array;
     public function findById(string $id): CarModel;
+    public function getCount(): int;
     public function findByName(string $name): array;
     public function findByMinInStock(int $minInStock): array; // find all cars where inStock >= minInStock
     public function save(string $id, string $name, int $price, int $inStock, string $createdAt, string $updatedAt): void;
