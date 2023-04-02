270dd959f89e3ff91b9c9af0fd3eba1688a0f29b
Rasolomanana Herimanitra Olivier <herimanitraolivierr@gmail.com>
2023-04-02 11:09:42 +0300

    fix(dummyData): update id to have 21 characters

--- dummyData/fake-clients.sql
+++ dummyData/fake-clients.sql
@@ -1,12 +1,12 @@
 INSERT INTO clients (id, name, contact) 
 VALUES 
-  ('ABC123', 'John Doe', '5551234567'),
-  ('DEF456', 'Jane Smith', '5555678901'),
-  ('GHI789', 'Bob Johnson', '5559012345'),
-  ('JKL012', 'Emily Davis', '5553456789'),
-  ('MNO345', 'Alex Brown', '5557890123'),
-  ('PQR678', 'Sarah Wilson', '5552345678'),
-  ('STU901', 'Tom Jackson', '5556789012'),
-  ('VWX234', 'Nancy Lee', '5550123456'),
-  ('YZA567', 'Adam Green', '5554567890'),
-  ('BCD890', 'Karen Chen', '5558901234');
+  ('A7ZRqbigOw4PYwglYdvel', 'John Doe', '5551234567'),
+  ('A7ZRqb9gOw4PYwglYdv16', 'Jane Smith', '5555678901'),
+  ('A7ZRqb9gOw4PYwglYdobl', 'Bob Johnson', '5559012345'),
+  ('AucRqb9gOw4PYwglYdvel', 'Emily Davis', '5553456789'),
+  ('tmxRqb9gOw4PYwglYdvel', 'Alex Brown', '5557890123'),
+  ('AkZRqb9gOw4PYwglYdvel', 'Sarah Wilson', '5552345678'),
+  ('A7ZRqb5gOw4PYwglYdvel', 'Tom Jackson', '5556789012'),
+  ('A7ZRqb9gOw4PYwelYdvel', 'Nancy Lee', '5550123456'),
+  ('A7ZRqb9opw4PYwglYdvel', 'Adam Green', '5554567890'),
+  ('A7ZRqkogOw4PYwglYdvel', 'Karen Chen', '5558901234');
