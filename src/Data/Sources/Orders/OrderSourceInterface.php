8ad120a1d396dd3ea7d424e8fe18de5d3d89970f
Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2023-04-02 17:52:05 +0300

    Feat: Add count and revenue methods to OrderSourceInterface * Add getCount method * Add getCountByLastMonths method * Add getRevenue method * Add getRevenueByLastMonths method

--- src/Data/Sources/Orders/OrderSourceInterface.php
+++ src/Data/Sources/Orders/OrderSourceInterface.php
@@ -9,6 +9,10 @@ interface OrderSourceInterface
 {
     public function findAll(?DateTime $startAt, ?DateTime $endAt): array;
     public function findById(string $id): OrderModel;
+    public function getCount(): int;
+    public function getCountByLastMonths(int $lastMonths): int;
+    public function getRevenue(): int;
+    public function getRevenueByLastMonths(int $lastMonths): int;
     public function findByClientId(string $clientId): array;
     public function save(string $id, string $clientId, array $orderCarsIds, array $carsIds, array $quantities, string $createdAt, string $updatedAt): void;
     public function update(string $id, string $clientId, array $orderCarsIds, array $carsIds, array $quantities, string $createdAt, string $updatedAt): void;
