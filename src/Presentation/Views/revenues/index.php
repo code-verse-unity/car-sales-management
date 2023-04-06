c8b82fc70e30702b3c7108c64ba9c4ecd8ad3077
Rasolomanana Herimanitra Olivier <herimanitraolivierr@gmail.com>
2023-04-06 11:52:19 +0300

    feat(revenues): add chart to display revenue per month for last 6 months The chart displays the revenue per month for the last 6 months. It uses Chart.js library to create a line chart. The chart is displayed on the revenues index page. The chart is accompanied by a list of revenue per month for the last 6 months. The list is displayed on the right side of the chart. The revenue per month is displayed in a div with a white background, rounded corners, and a shadow. The revenue per month is displayed in a bold font and formatted as currency using the FormatCurrency class.

--- src/Presentation/Views/revenues/index.php
+++ src/Presentation/Views/revenues/index.php
@@ -1,12 +1,61 @@
 <?php
+
 use App\Core\Utils\Strings\FormatCurrency;
 ?>
 
-<ul>
-    <?php foreach($revenuePerMonthForLast6Months as $dateAmount): ?>
-        <li>
-            <p><?= $dateAmount["date"]->format("F Y") ?></p>
-            <p><?= FormatCurrency::format($dateAmount["amount"]) ?></p>
-        </li>
-    <?php endforeach; ?>
-</ul>
+<main class="my-4">
+    <h1 class="fw-bold w-50 mb-4"><span class="text-orange">Recette</span> totale accumulé des <span class="text-orange">6 derniers mois</span>.</h1>
+    <div class="row">
+        <div class="col-8 ">
+            <div class="bg-white p-3 rounded-3 shadow sticky-top">
+                <canvas id="myChart"></canvas>
+            </div>
+        </div>
+        <div class="col">
+            <?php foreach ($revenuePerMonthForLast6Months as $dateAmount) : ?>
+                <div class="bg-white p-3 mb-3 rounded-3 shadow">
+                    <h6><?= $dateAmount["date"]->format("F Y") ?></h6>
+                    <p class="fw-bold fs-4 text-orange"><?= FormatCurrency::format($dateAmount["amount"]) ?></p>
+                </div>
+            <?php endforeach; ?>
+
+        </div>
+    </div>
+
+</main>
+
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+<script>
+    const ctx = document.getElementById('myChart');
+
+    const MONTHS = ['Janvier', 'Fevrier', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Aout', 'Septembre', 'Octobre', 'Novembre', 'Decembre']
+
+    const revenues = <?= json_encode($revenuePerMonthForLast6Months) ?>;
+    const dates = revenues.map((revenue) => MONTHS[new Date(revenue.date.date).getMonth()]).reverse();
+    const data = revenues.map((revenue) => revenue.amount).reverse();
+
+    new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: dates,
+            datasets: [{
+                borderWidth: 1,
+                label: 'Recettes par mois',
+                fill: false,
+                borderColor: '#ff953f',
+                tension: 0.1,
+                borderWidth: 3,
+                data,
+            }],
+
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+</script>
