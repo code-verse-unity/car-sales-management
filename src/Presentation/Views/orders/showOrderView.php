6b3d6b1f1c52fdcfeceaa2ca56e31f81e879b89f
Rasolomanana Herimanitra Olivier <herimanitraolivierr@gmail.com>
2023-04-07 07:57:00 +0300

    refactor(showOrderView.php): remove commented out code and unused HTML The commented out code and unused HTML were removed to improve the readability and maintainability of the code.

--- src/Presentation/Views/orders/showOrderView.php
+++ src/Presentation/Views/orders/showOrderView.php
@@ -3,22 +3,6 @@
 use App\Core\Utils\Strings\FormatCurrency;
 ?>
 
-<?php
-// ob_start();
-// require_once __DIR__ . "/../_bill.php"; // a preview of the bill
-// echo ob_get_clean();
-?>
-
-<!-- <form action="/orders/<?= $order["id"] ?>/delete" method="POST">
-    <input type="submit" value="Annuler cet achat" style="color:red;">
-</form>
-
-<a href="/bills/<?= $order["id"] ?>/download">Télécharger la facture</a> -->
-
-<!-- <pre>
-    <?php print_r($order) ?>
-</pre> -->
-
 <main>
     <div class="row my-3">
         <div class="col-9">
