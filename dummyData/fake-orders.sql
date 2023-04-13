c569133973ab6620766c6f43f688a848140fa53e
Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2023-04-13 16:58:43 +0300

    Feat: Add fake data

--- dummyData/fake-orders.sql
+++ dummyData/fake-orders.sql
@@ -1,5 +1,674 @@
-INSERT INTO orders (id, clientId) 
-VALUES ('A7rt156v4PYwglYdvelks','A7ZRqb5gOw4PYwglYdvel');
-
-INSERT INTO order_cars(id, orderId, carId, quantity)
-VALUES ('A7rt156v4PYwo16fhelks','A7rt156v4PYwglYdvelks','A7rtqb9gOw4PYwglYdvel',1);
+INSERT INTO `orders`
+VALUES (
+        '0CJHZQsxguoZahLVw9yJu',
+        'pfSSKguJIlISCaQuZyD93',
+        '2023-01-08 13:28:46',
+        '2023-01-08 13:28:46'
+    ),
+    (
+        '5DBEmYTf6XG9SoSLOkzvk',
+        'ZyNhnm94p4wgDjZQyNc8k',
+        '2022-11-03 12:00:58',
+        '2022-11-03 12:00:58'
+    ),
+    (
+        '8obxx07skzwlV6WmILkjF',
+        'wG_5xmZvBWMm6YooDLIK2',
+        '2023-01-31 23:08:49',
+        '2023-01-31 23:08:49'
+    ),
+    (
+        '9MtnEmp8izt1URGPbnxl9',
+        'p3glj5z_03TcFW53GpXzZ',
+        '2022-11-12 07:41:33',
+        '2022-11-12 07:41:33'
+    ),
+    (
+        'Chch7joSkWxwlrj2mCdpF',
+        'hWQOQgVnvkntnnbwjUBQ3',
+        '2022-11-15 02:39:38',
+        '2022-11-15 02:39:38'
+    ),
+    (
+        'dZ4VDbwO0vJuLXZlpoAPF',
+        'pfSSKguJIlISCaQuZyD93',
+        '2023-04-02 20:30:48',
+        '2023-04-02 20:30:48'
+    ),
+    (
+        'G86p6uja2nyjr7I8IPhNs',
+        'p3glj5z_03TcFW53GpXzZ',
+        '2023-03-30 23:01:58',
+        '2023-03-30 23:01:58'
+    ),
+    (
+        'HSVcg0Pz61vlYjIGzyyvQ',
+        'pfSSKguJIlISCaQuZyD93',
+        '2023-04-06 00:05:01',
+        '2023-04-06 00:05:01'
+    ),
+    (
+        'LNLB5BvCrcWPvZjagbu8F',
+        'pfSSKguJIlISCaQuZyD93',
+        '2022-10-30 03:28:25',
+        '2022-10-30 03:28:25'
+    ),
+    (
+        'MJOliJiv21U7wMyt49M7b',
+        'TJSPQUqDMYcGGacYsHGML',
+        '2023-03-25 01:16:51',
+        '2023-03-25 01:16:51'
+    ),
+    (
+        'N8ERXigpRKXQUo32d4XO_',
+        'EIPl7pnPLtCLZjJZ4isZj',
+        '2022-11-04 09:28:40',
+        '2022-11-04 09:28:40'
+    ),
+    (
+        'oE7GwF66knT6_0rATxFUE',
+        'hWQOQgVnvkntnnbwjUBQ3',
+        '2022-11-10 11:45:13',
+        '2022-11-10 11:45:13'
+    ),
+    (
+        'ooV4wPOMgf63IbLN8P7c2',
+        'p3glj5z_03TcFW53GpXzZ',
+        '2023-01-24 03:36:27',
+        '2023-01-24 03:36:27'
+    ),
+    (
+        'oPqBNn_OWgBVPDSIhwjz9',
+        'ZyNhnm94p4wgDjZQyNc8k',
+        '2023-02-21 02:13:01',
+        '2023-02-21 02:13:01'
+    ),
+    (
+        'Q3_SOpv6VAFl94BKla7RZ',
+        'EIPl7pnPLtCLZjJZ4isZj',
+        '2023-01-08 14:40:05',
+        '2023-01-08 14:40:05'
+    ),
+    (
+        'Qu4sv5yhgyWvHBYpoqaP6',
+        'ZyNhnm94p4wgDjZQyNc8k',
+        '2022-11-11 03:21:07',
+        '2022-11-11 03:21:07'
+    ),
+    (
+        'R1kru7OJlfcHabuMnNyOm',
+        'ZyNhnm94p4wgDjZQyNc8k',
+        '2023-02-13 12:00:58',
+        '2023-02-13 12:00:58'
+    ),
+    (
+        'VjNQ7Ok65a7e_mpcnV34N',
+        'ZyNhnm94p4wgDjZQyNc8k',
+        '2023-02-04 22:32:09',
+        '2023-02-04 22:32:09'
+    ),
+    (
+        'vMfZd9j0lBUiAJ5SM9sQs',
+        'EIPl7pnPLtCLZjJZ4isZj',
+        '2023-03-25 17:49:49',
+        '2023-03-25 17:49:49'
+    ),
+    (
+        'xqMTlbkDnhwp0WwDN2Lg9',
+        'TJSPQUqDMYcGGacYsHGML',
+        '2023-02-23 07:50:52',
+        '2023-02-23 07:50:52'
+    );
+INSERT INTO `order_cars`
+VALUES (
+        '_Eiud1UwrpcR3sUTRoLUc',
+        'MJOliJiv21U7wMyt49M7b',
+        'eGjfMkBO28nB5CABQ89yg',
+        5,
+        '2023-03-25 01:16:51',
+        '2023-03-25 01:16:51'
+    ),
+(
+        '_rRVzIDhm_GBJAyhryjPy',
+        '5DBEmYTf6XG9SoSLOkzvk',
+        'ukDaE9oEHICj4HQxWqrGa',
+        4,
+        '2022-11-03 12:00:58',
+        '2022-11-03 12:00:58'
+    ),
+(
+        '0a6RzrXOiXy2UxMfoUbjy',
+        'oPqBNn_OWgBVPDSIhwjz9',
+        '60acuLFcZLaN4Cp40a_Vu',
+        5,
+        '2023-02-21 02:13:01',
+        '2023-02-21 02:13:01'
+    ),
+(
+        '2wooFqCOL91BbAPepcVSU',
+        'HSVcg0Pz61vlYjIGzyyvQ',
+        'V5272AwTHUWxbEjdMh9EE',
+        3,
+        '2023-04-06 00:05:01',
+        '2023-04-06 00:05:01'
+    ),
+(
+        '35LLmjBNVxCTLA27b7Fmc',
+        'xqMTlbkDnhwp0WwDN2Lg9',
+        'eGjfMkBO28nB5CABQ89yg',
+        2,
+        '2023-02-23 07:50:52',
+        '2023-02-23 07:50:52'
+    ),
+(
+        '3PvbobWpn8lqa2u0diEG0',
+        '5DBEmYTf6XG9SoSLOkzvk',
+        'kD3EMijlljTlt1FPbg7uI',
+        1,
+        '2022-11-03 12:00:58',
+        '2022-11-03 12:00:58'
+    ),
+(
+        '3ymHjGMjwla6YFfzS7dAX',
+        'LNLB5BvCrcWPvZjagbu8F',
+        'V5272AwTHUWxbEjdMh9EE',
+        2,
+        '2022-10-30 03:28:25',
+        '2022-10-30 03:28:25'
+    ),
+(
+        '42nJLotEeYrpeh5YpSGdg',
+        'G86p6uja2nyjr7I8IPhNs',
+        'w53L0KGGmKX6MWciUcC6I',
+        6,
+        '2023-03-30 23:01:58',
+        '2023-03-30 23:01:58'
+    ),
+(
+        '4NRs7fx1FHEAIaFJDnNBo',
+        'Chch7joSkWxwlrj2mCdpF',
+        '4NU9H0jIIUUQs0XoYg0a8',
+        3,
+        '2022-11-15 02:39:38',
+        '2022-11-15 02:39:38'
+    ),
+(
+        '6tU0RuHGoYV4yX40Pj09y',
+        'oE7GwF66knT6_0rATxFUE',
+        '8bKNeq_EM24UErIrSReUM',
+        1,
+        '2022-11-10 11:45:13',
+        '2022-11-10 11:45:13'
+    ),
+(
+        '8R7eVnjPDkSqTY_qfZHOR',
+        'G86p6uja2nyjr7I8IPhNs',
+        'V5272AwTHUWxbEjdMh9EE',
+        6,
+        '2023-03-30 23:01:58',
+        '2023-03-30 23:01:58'
+    ),
+(
+        '8tRgkVI6rZ4d1eHlRZjUJ',
+        'Q3_SOpv6VAFl94BKla7RZ',
+        'eGjfMkBO28nB5CABQ89yg',
+        4,
+        '2023-01-08 14:40:05',
+        '2023-01-08 14:40:05'
+    ),
+(
+        '8WhOanPE6a6HEkZMPaEHf',
+        '5DBEmYTf6XG9SoSLOkzvk',
+        'w53L0KGGmKX6MWciUcC6I',
+        6,
+        '2022-11-03 12:00:58',
+        '2022-11-03 12:00:58'
+    ),
+(
+        '9cw2KqXOb9MXJtyBiM0Wd',
+        'N8ERXigpRKXQUo32d4XO_',
+        'eGjfMkBO28nB5CABQ89yg',
+        5,
+        '2022-11-04 09:28:40',
+        '2022-11-04 09:28:40'
+    ),
+(
+        '9qiKHYQhaladMUcYyVbgU',
+        'LNLB5BvCrcWPvZjagbu8F',
+        'YUnHUbxo7GYjb16uyGySF',
+        2,
+        '2022-10-30 03:28:25',
+        '2022-10-30 03:28:25'
+    ),
+(
+        'an5EjsryHQ7XXSB1FCuGq',
+        'LNLB5BvCrcWPvZjagbu8F',
+        'kD3EMijlljTlt1FPbg7uI',
+        1,
+        '2022-10-30 03:28:25',
+        '2022-10-30 03:28:25'
+    ),
+(
+        'AwjQrGlGSsB30pNsCgpsG',
+        'dZ4VDbwO0vJuLXZlpoAPF',
+        'ukDaE9oEHICj4HQxWqrGa',
+        6,
+        '2023-04-02 20:30:48',
+        '2023-04-02 20:30:48'
+    ),
+(
+        'BEGlE33bZe92RzyggpQwI',
+        'ooV4wPOMgf63IbLN8P7c2',
+        'eGjfMkBO28nB5CABQ89yg',
+        6,
+        '2023-01-24 03:36:27',
+        '2023-01-24 03:36:27'
+    ),
+(
+        'CbKIPK0v4sL1UJd_qJXDn',
+        'MJOliJiv21U7wMyt49M7b',
+        'r8Bmd5uO8OUAgB_hMZ3vs',
+        5,
+        '2023-03-25 01:16:51',
+        '2023-03-25 01:16:51'
+    ),
+(
+        'CoqD01RSymqVG6BZKhQB0',
+        'Qu4sv5yhgyWvHBYpoqaP6',
+        '8bKNeq_EM24UErIrSReUM',
+        1,
+        '2022-11-11 03:21:07',
+        '2022-11-11 03:21:07'
+    ),
+(
+        'DMsKbiqNQk8ty3vM0xaA4',
+        '9MtnEmp8izt1URGPbnxl9',
+        'THmWU4vgwRcE8KA_oigny',
+        3,
+        '2022-11-12 07:41:33',
+        '2022-11-12 07:41:33'
+    ),
+(
+        'Ea5Q5Jszwm3uCHDv1Cds4',
+        'VjNQ7Ok65a7e_mpcnV34N',
+        '3gz8WcuoyaTnV6Cilw6Dx',
+        4,
+        '2023-02-04 22:32:09',
+        '2023-02-04 22:32:09'
+    ),
+(
+        'eDALYSpciw55baNCB3oKD',
+        '9MtnEmp8izt1URGPbnxl9',
+        '3T0NAjY_ZS2bWMmGmwJfz',
+        6,
+        '2022-11-12 07:41:33',
+        '2022-11-12 07:41:33'
+    ),
+(
+        'eR2qjlBxc0GzjXZ6gmnCt',
+        'VjNQ7Ok65a7e_mpcnV34N',
+        '3T0NAjY_ZS2bWMmGmwJfz',
+        1,
+        '2023-02-04 22:32:09',
+        '2023-02-04 22:32:09'
+    ),
+(
+        'h6kBijqGQGpGbzQzKy84D',
+        '5DBEmYTf6XG9SoSLOkzvk',
+        'YUnHUbxo7GYjb16uyGySF',
+        4,
+        '2022-11-03 12:00:58',
+        '2022-11-03 12:00:58'
+    ),
+(
+        'h6Yy5ldI6mDvSJx3VYeXe',
+        '9MtnEmp8izt1URGPbnxl9',
+        '3gz8WcuoyaTnV6Cilw6Dx',
+        4,
+        '2022-11-12 07:41:33',
+        '2022-11-12 07:41:33'
+    ),
+(
+        'HgGLiyVicW7JBqcWQ2kw9',
+        'Chch7joSkWxwlrj2mCdpF',
+        'HxH_1ogO55kQHfE5Myo1N',
+        2,
+        '2022-11-15 02:39:38',
+        '2022-11-15 02:39:38'
+    ),
+(
+        'hkHATrZklmIJVwi2sES_Y',
+        'LNLB5BvCrcWPvZjagbu8F',
+        'uSHJzEeh3TuEDoIei1b5w',
+        5,
+        '2022-10-30 03:28:25',
+        '2022-10-30 03:28:25'
+    ),
+(
+        'hl7bxSyNk49wuD90oxV7A',
+        'Chch7joSkWxwlrj2mCdpF',
+        'V5272AwTHUWxbEjdMh9EE',
+        6,
+        '2022-11-15 02:39:38',
+        '2022-11-15 02:39:38'
+    ),
+(
+        'hP_gph2NXGvWkh5dXnJH3',
+        'G86p6uja2nyjr7I8IPhNs',
+        'YUnHUbxo7GYjb16uyGySF',
+        1,
+        '2023-03-30 23:01:58',
+        '2023-03-30 23:01:58'
+    ),
+(
+        'Iv64LbYn_i8Lg7ZxhZY_C',
+        'G86p6uja2nyjr7I8IPhNs',
+        '4NU9H0jIIUUQs0XoYg0a8',
+        1,
+        '2023-03-30 23:01:58',
+        '2023-03-30 23:01:58'
+    ),
+(
+        'J_wRAAEm8iEgAOBIxn6vo',
+        'Qu4sv5yhgyWvHBYpoqaP6',
+        'HxH_1ogO55kQHfE5Myo1N',
+        5,
+        '2022-11-11 03:21:07',
+        '2022-11-11 03:21:07'
+    ),
+(
+        'KOfuRZ6ppGD1HgY49mQlZ',
+        'xqMTlbkDnhwp0WwDN2Lg9',
+        'r8Bmd5uO8OUAgB_hMZ3vs',
+        3,
+        '2023-02-23 07:50:52',
+        '2023-02-23 07:50:52'
+    ),
+(
+        'KzfDyr4zrUMrvxvhpd37m',
+        '9MtnEmp8izt1URGPbnxl9',
+        '8bKNeq_EM24UErIrSReUM',
+        3,
+        '2022-11-12 07:41:33',
+        '2022-11-12 07:41:33'
+    ),
+(
+        'L6g8tAKLRAvk_Q6FzJkk1',
+        'vMfZd9j0lBUiAJ5SM9sQs',
+        '4NU9H0jIIUUQs0XoYg0a8',
+        4,
+        '2023-03-25 17:49:49',
+        '2023-03-25 17:49:49'
+    ),
+(
+        'lqNup_BCQ4c5h556LbpZX',
+        'Chch7joSkWxwlrj2mCdpF',
+        'ukDaE9oEHICj4HQxWqrGa',
+        5,
+        '2022-11-15 02:39:38',
+        '2022-11-15 02:39:38'
+    ),
+(
+        'lrSkcyjUp9PwLankmmLMG',
+        'ooV4wPOMgf63IbLN8P7c2',
+        '60acuLFcZLaN4Cp40a_Vu',
+        1,
+        '2023-01-24 03:36:27',
+        '2023-01-24 03:36:27'
+    ),
+(
+        'm3PEBaKniCOceIs3ZMUof',
+        '5DBEmYTf6XG9SoSLOkzvk',
+        'kD3EMijlljTlt1FPbg7uI',
+        2,
+        '2022-11-03 12:00:58',
+        '2022-11-03 12:00:58'
+    ),
+(
+        'MDMS7hIv1SExQVRa3BK2_',
+        'Q3_SOpv6VAFl94BKla7RZ',
+        'YUnHUbxo7GYjb16uyGySF',
+        6,
+        '2023-01-08 14:40:05',
+        '2023-01-08 14:40:05'
+    ),
+(
+        'Mp139DwU94fbwykZ_x9SO',
+        'HSVcg0Pz61vlYjIGzyyvQ',
+        'HxH_1ogO55kQHfE5Myo1N',
+        4,
+        '2023-04-06 00:05:01',
+        '2023-04-06 00:05:01'
+    ),
+(
+        'N2V_GQ2IjOQZE3c94iP05',
+        'oE7GwF66knT6_0rATxFUE',
+        '3T0NAjY_ZS2bWMmGmwJfz',
+        5,
+        '2022-11-10 11:45:13',
+        '2022-11-10 11:45:13'
+    ),
+(
+        'n5HpEbeapKz4O9VOTL8FQ',
+        'VjNQ7Ok65a7e_mpcnV34N',
+        '4NU9H0jIIUUQs0XoYg0a8',
+        5,
+        '2023-02-04 22:32:09',
+        '2023-02-04 22:32:09'
+    ),
+(
+        'o6tYgEy9MLQodgjenTtGy',
+        '8obxx07skzwlV6WmILkjF',
+        'YUnHUbxo7GYjb16uyGySF',
+        5,
+        '2023-01-31 23:08:49',
+        '2023-01-31 23:08:49'
+    ),
+(
+        'ofU7fcfhdJrE31NzCD8kg',
+        'VjNQ7Ok65a7e_mpcnV34N',
+        'eGjfMkBO28nB5CABQ89yg',
+        4,
+        '2023-02-04 22:32:09',
+        '2023-02-04 22:32:09'
+    ),
+(
+        'OQQe0m2mwbxjAYuSf4Ifm',
+        '0CJHZQsxguoZahLVw9yJu',
+        'uSHJzEeh3TuEDoIei1b5w',
+        6,
+        '2023-01-08 13:28:46',
+        '2023-01-08 13:28:46'
+    ),
+(
+        'OrF8nCiMb3PDSb3NYC4Q8',
+        'xqMTlbkDnhwp0WwDN2Lg9',
+        '3T0NAjY_ZS2bWMmGmwJfz',
+        3,
+        '2023-02-23 07:50:52',
+        '2023-02-23 07:50:52'
+    ),
+(
+        'oRpo4caHzpmCkauFo8wS1',
+        'ooV4wPOMgf63IbLN8P7c2',
+        'V5272AwTHUWxbEjdMh9EE',
+        1,
+        '2023-01-24 03:36:27',
+        '2023-01-24 03:36:27'
+    ),
+(
+        'PWujObszDQBTvPNNW2Yka',
+        'G86p6uja2nyjr7I8IPhNs',
+        'uSHJzEeh3TuEDoIei1b5w',
+        4,
+        '2023-03-30 23:01:58',
+        '2023-03-30 23:01:58'
+    ),
+(
+        'QUFu5kwa9pju17L24V_GU',
+        'oE7GwF66knT6_0rATxFUE',
+        'uSHJzEeh3TuEDoIei1b5w',
+        6,
+        '2022-11-10 11:45:13',
+        '2022-11-10 11:45:13'
+    ),
+(
+        'rc66KQq2CYTDtLiGY6I0c',
+        'G86p6uja2nyjr7I8IPhNs',
+        'w53L0KGGmKX6MWciUcC6I',
+        1,
+        '2023-03-30 23:01:58',
+        '2023-03-30 23:01:58'
+    ),
+(
+        'rFiKMFYxUuiWcEdw3lbde',
+        'VjNQ7Ok65a7e_mpcnV34N',
+        '8bKNeq_EM24UErIrSReUM',
+        5,
+        '2023-02-04 22:32:09',
+        '2023-02-04 22:32:09'
+    ),
+(
+        'RKwK8CW1vUF6kCAsXBgyt',
+        'Qu4sv5yhgyWvHBYpoqaP6',
+        'uSHJzEeh3TuEDoIei1b5w',
+        1,
+        '2022-11-11 03:21:07',
+        '2022-11-11 03:21:07'
+    ),
+(
+        'UIizWxfaTPZdibAzQ430x',
+        'dZ4VDbwO0vJuLXZlpoAPF',
+        '60acuLFcZLaN4Cp40a_Vu',
+        2,
+        '2023-04-02 20:30:48',
+        '2023-04-02 20:30:48'
+    ),
+(
+        'ULHNfIH0yECfldgIO360q',
+        '9MtnEmp8izt1URGPbnxl9',
+        'eGjfMkBO28nB5CABQ89yg',
+        4,
+        '2022-11-12 07:41:33',
+        '2022-11-12 07:41:33'
+    ),
+(
+        'V6BuXETX0jME6p_duwv9Q',
+        'ooV4wPOMgf63IbLN8P7c2',
+        'HxH_1ogO55kQHfE5Myo1N',
+        1,
+        '2023-01-24 03:36:27',
+        '2023-01-24 03:36:27'
+    ),
+(
+        'VnXTNrFJ4xI8y6dBqbfJ9',
+        'Q3_SOpv6VAFl94BKla7RZ',
+        'w53L0KGGmKX6MWciUcC6I',
+        4,
+        '2023-01-08 14:40:05',
+        '2023-01-08 14:40:05'
+    ),
+(
+        'voI1h0ClDtpEh3b3tI_sm',
+        'oE7GwF66knT6_0rATxFUE',
+        '60acuLFcZLaN4Cp40a_Vu',
+        1,
+        '2022-11-10 11:45:13',
+        '2022-11-10 11:45:13'
+    ),
+(
+        'Vqgem0U6BpdxexHFuFEzQ',
+        'MJOliJiv21U7wMyt49M7b',
+        '3gz8WcuoyaTnV6Cilw6Dx',
+        1,
+        '2023-03-25 01:16:51',
+        '2023-03-25 01:16:51'
+    ),
+(
+        'W7MnRt0G2vufmL6A0VRe0',
+        'Chch7joSkWxwlrj2mCdpF',
+        '60acuLFcZLaN4Cp40a_Vu',
+        6,
+        '2022-11-15 02:39:38',
+        '2022-11-15 02:39:38'
+    ),
+(
+        'WnovzjafOMFPzN1r0wNIp',
+        'R1kru7OJlfcHabuMnNyOm',
+        'eGjfMkBO28nB5CABQ89yg',
+        6,
+        '2023-02-13 12:00:58',
+        '2023-02-13 12:00:58'
+    ),
+(
+        'Wydsc2Y49XuxNucjDEwmd',
+        'LNLB5BvCrcWPvZjagbu8F',
+        'ukDaE9oEHICj4HQxWqrGa',
+        4,
+        '2022-10-30 03:28:25',
+        '2022-10-30 03:28:25'
+    ),
+(
+        'XjnGmhwjq9TqR0XP7_Zx4',
+        'Chch7joSkWxwlrj2mCdpF',
+        '60acuLFcZLaN4Cp40a_Vu',
+        1,
+        '2022-11-15 02:39:38',
+        '2022-11-15 02:39:38'
+    ),
+(
+        'XK7_MFdUYNDoIJ2ZFvC16',
+        'vMfZd9j0lBUiAJ5SM9sQs',
+        'ukDaE9oEHICj4HQxWqrGa',
+        6,
+        '2023-03-25 17:49:49',
+        '2023-03-25 17:49:49'
+    ),
+(
+        'XQSBevQ5NhYyrSy31Jm7d',
+        'xqMTlbkDnhwp0WwDN2Lg9',
+        'eGjfMkBO28nB5CABQ89yg',
+        3,
+        '2023-02-23 07:50:52',
+        '2023-02-23 07:50:52'
+    ),
+(
+        'xWO6Zj3RBLwTBaOuwYKvU',
+        'HSVcg0Pz61vlYjIGzyyvQ',
+        '4NU9H0jIIUUQs0XoYg0a8',
+        5,
+        '2023-04-06 00:05:01',
+        '2023-04-06 00:05:01'
+    ),
+(
+        'z2QnLzT5wOyR05KwROo19',
+        'R1kru7OJlfcHabuMnNyOm',
+        'V5272AwTHUWxbEjdMh9EE',
+        2,
+        '2023-02-13 12:00:58',
+        '2023-02-13 12:00:58'
+    ),
+(
+        'z6OdJXODzDbNsYcVvcznn',
+        'xqMTlbkDnhwp0WwDN2Lg9',
+        'V5272AwTHUWxbEjdMh9EE',
+        5,
+        '2023-02-23 07:50:52',
+        '2023-02-23 07:50:52'
+    ),
+(
+        'zq4PwO4yvZp_9NXL3s9bM',
+        '0CJHZQsxguoZahLVw9yJu',
+        '3gz8WcuoyaTnV6Cilw6Dx',
+        5,
+        '2023-01-08 13:28:46',
+        '2023-01-08 13:28:46'
+    ),
+(
+        'ZyaSjGJuPi1vdPoad7lGa',
+        'ooV4wPOMgf63IbLN8P7c2',
+        '60acuLFcZLaN4Cp40a_Vu',
+        4,
+        '2023-01-24 03:36:27',
+        '2023-01-24 03:36:27'
+    );
